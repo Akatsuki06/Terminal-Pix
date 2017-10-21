ff2f7be1a7900d8eccb9564ef361e7874aafa77b
akatsuki06 <suraznegi@gmail.com>
2017-10-21 22:43:30 +0530

    added utils

--- main.py
+++ main.py
@@ -3,54 +3,23 @@ import sys
 from PIL import Image
 from color import *
 import scipy.misc as sm
-import getopt
-import os
-# img = cv2.imread('flag.jpg')
+from util import getArgs,errorFileNotFound,help
 
+file,h,w=getArgs()
+size=(int(h),int(w))
 
-# size = (20, 40)
-try:
-    myopts, args = getopt.getopt(sys.argv[1:],"f:h:w:")
-except getopt.GetoptError as e:
-    print (str(e))
-    print("Usage: %s " % sys.argv[0])
-    sys.exit(2)
-file=''
-h=50
-w=50
-for o, a in myopts:
-    if o == '-f':
-        file=a
-    elif o == '-h':
-        h=a
-    elif o=='-w':
-    	w=a
-
-r, c = os.popen('stty size', 'r').read().split()
-r=int(r)
-c=int(c)
-print(r,c)
-filenotfound=' \n Please provide filename using -f <filename>!!'
 if not file:
-	print(int(c)*"*")
-	e1='Error no file argument found!'
-	sp=int((c-len(e1))/2)
-	print('*',sp*" ",e1)
-	e1="Please provide filename using -f <filename>!!"
-	sp=sp=int((c-len(e1))/2)
-	print('*',sp*" ",e1)
-	print(int(c)*"*")
-	exit(0)
-
-size=(h,w)
+	errorFileArgNotFound()
 print(file,size)
-exit(0)
+# exit(0)
+try:
+    img=Image.open(filename)
+    # do stuff
+except IOError:
+	errorFileNotFound():
 
 img = np.array(Image.open(file))
 img = sm.imresize(img,size)
-# img=cv2.resize(img, (400, 200)) 
-# img = cv2.GaussianBlur(img,(5,5),0)
-# ret3,img = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 l=img.shape
 # print(img[0][0])
 for i in range(0,l[0]):
---
+++ util.py
@@ -0,0 +1,53 @@
+import sys
+import getopt
+import os
+
+
+def getArgs():
+	try: 
+		opts, args = getopt.getopt(sys.argv[1:],"f:h:w:",['help'])
+	except getopt.GetoptError as e:
+		print (str(e))
+		print("Usage: %s " % sys.argv[0])
+		sys.exit(2)
+	file=''
+	h=50
+	w=50
+	for o, a in opts:
+		if o=='--help' :
+			help()
+		if o == '-f':
+		   	file=a
+		if o == '-h':
+		    h=a
+		if o =='-w':
+			w=a
+
+
+	return file,h,w
+
+
+def errorFileArgNotFound():
+	fileargnotfound=['Error no file argument found!','Please provide filename using \'-f <filename>\'.']
+	fileError(fileargnotfound)
+
+def errorFileNotFound:
+	filenotfound=['File not found!','Please provide the correct using filename with path.']
+	fileError(filenotfound)
+	
+
+def fileError(filenotfound):
+	r, c = os.popen('stty size', 'r').read().split()
+	r=int(r)
+	c=int(c)
+	print(r,c)
+	print(int(c)*"*")
+	for e in filenotfound:
+		sp=int((c-len(e))/2)-1
+		print(sp*" ",e,sp*" ")
+	print(int(c)*"*")
+	exit(0)
+
+def help():
+	print('helping...')
+	exit(0)
